a30a33b8fbf42c19583b86c0206a17625558e0e9
Divya <divyabhatt16@gmail.com>
2022-02-09 14:37:29 +0000

    Exercise 1 finished coding and testing

--- src/main/scala/com/techreturners/exercise001/Exercise001.scala
+++ src/main/scala/com/techreturners/exercise001/Exercise001.scala
@@ -1,5 +1,9 @@
 package com.techreturners.exercise001
 
+import scala.collection.immutable.{AbstractSeq, LinearSeq}
+import scala.collection.mutable.ArrayBuffer
+import scala.math.BigDecimal.RoundingMode
+
 class Exercise001 {
 
   // Scala methods defined in various styles
@@ -7,13 +11,13 @@ class Exercise001 {
   // we don't specify it in the code
   // https://docs.scala-lang.org/tour/basics.html
 
-  def capitalizeWord(word: String): String = ""
+  def capitalizeWord(word: String): String = word.substring(0,1).toUpperCase()+word.substring(1,word.length)
 
-  def generateInitials(firstName: String, lastName: String) = ""
+  def generateInitials(firstName: String, lastName: String) = firstName.substring(0,1)+"."+lastName.substring(0,1)
 
-  def addVat(initialPrice: Double, interestRate: Double) : Double = 0.0
+  def addVat(initialPrice: Double, interestRate: Double) : Double = BigDecimal(((initialPrice*interestRate)/100 + initialPrice)).setScale(2, RoundingMode.HALF_EVEN).doubleValue
 
-  def reverse(sentence: String) : String = ""
+  def reverse(sentence: String) : String = sentence.reverse
 
   /**
    * Here we show using a Scala sequence which is a type of collection
@@ -22,9 +26,19 @@ class Exercise001 {
    * @param users A collection of users
    * @return
    */
-  def countLinuxUsers(users: Seq[User]): Int = {
+  def countLinuxUsers(users: Seq[User]): Int =
+  {
     // Add your code here
-    0
+    var counts: Int = 0
+    if(users != null){
+      for( i <- 1 to users.length-1) {
+        if (users(i).osType == "Linux") {
+          counts += 1
+        } else
+          counts = 0
+      }
+    }
+    counts
   }
 
 }
