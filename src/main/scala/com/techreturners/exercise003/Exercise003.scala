2e69b914c29e36641e430691dbc61ca10323becf
Divya <divyabhatt16@gmail.com>
2022-02-10 11:32:30 +0000

    Exercise 3 finished coding and testing

--- src/main/scala/com/techreturners/exercise003/Exercise003.scala
+++ src/main/scala/com/techreturners/exercise003/Exercise003.scala
@@ -2,12 +2,17 @@ package com.techreturners.exercise003
 
 class Exercise003 {
 
-  def getIceCreamCode(iceCreamFlavour: String): Int = {
-    throw new UnsupportedOperationException("You can delete this line and add your code here.")
-  }
+  @ throws("UnsupportedOperationException")
+  def getIceCreamCode(iceCreamFlavour: String): Int = iceCreamFlavours.indexOf(iceCreamFlavour)
 
-  def iceCreamFlavours: Array[String] = {
-    throw new UnsupportedOperationException("You can delete this line and add your code here.")
-  }
+  @ throws("UnsupportedOperationException")/*Assuming if array is empty might throw unsupported exception hence defining as mutable arraybuffer */
+  def iceCreamFlavours: Array[String] = scala.collection.mutable.ArrayBuffer("Pistachio",
+    "Raspberry Ripple",
+    "Vanilla",
+    "Mint Chocolate Chip",
+    "Chocolate",
+    "Mango Sorbet"
+  ).toArray
+
 
 }
