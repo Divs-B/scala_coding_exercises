cd6129f1fb446d8a9c81f7b46d55ef4a3b44bef7
Divya <divyabhatt16@gmail.com>
2022-02-10 21:42:40 +0000

    Exercise 2 finished refactoring by removing if else statements.

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -3,9 +3,9 @@ package com.techreturners.exercise002
 class Exercise002 {
   val cityToCheck:String = "Manchester"
   val ageLimitToCheck: Int = 15
-  def isFromManchester(person: Person): Boolean = if(person.city.contentEquals(cityToCheck)) true else false
+  def isFromManchester(person: Person): Boolean = person.city.contentEquals(cityToCheck)
 
-  def canWatchFilm(person: Person, ageLimit: Int): Boolean = if(person.ageLimit>ageLimitToCheck) true else false
+  def canWatchFilm(person: Person, ageLimit: Int): Boolean = person.ageLimit>ageLimitToCheck
 
 
 }
