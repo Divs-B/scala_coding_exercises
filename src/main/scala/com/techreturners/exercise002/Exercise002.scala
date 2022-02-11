d147bcbd61699fe3459994cce057f71af1ba2ca8
Divya <divyabhatt16@gmail.com>
2022-02-11 00:04:15 +0000

    Exercise 2 implemented the correct logic in second method and also added one more test in test file.

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -2,11 +2,10 @@ package com.techreturners.exercise002
 
 class Exercise002 {
   val cityToCheck: String = "Manchester"
-  val ageLimitToCheck: Int = 15
 
   def isFromManchester(person: Person): Boolean = person.city.contentEquals(cityToCheck)
 
-  def canWatchFilm(person: Person, ageLimit: Int): Boolean = person.ageLimit > ageLimitToCheck
+  def canWatchFilm(person: Person, ageLimit: Int): Boolean = person.ageLimit > ageLimit
 
 
 }
--- src/test/scala/com/techreturners/exercise002/Exercise002Test.scala
+++ src/test/scala/com/techreturners/exercise002/Exercise002Test.scala
@@ -15,7 +15,7 @@ class Exercise002Test extends AnyFlatSpec {
   it should "be correctly identified as residing in somewhere other than Manchester" in {
     val person = Person("Susan", "Farmer", "Leeds", 23)
 
-    assert(!exercise002.isFromManchester( person))
+    assert(!exercise002.isFromManchester(person))
   }
 
   "A person" should "be correctly identified as being able to watch an over 18 film" in {
@@ -28,6 +28,16 @@ class Exercise002Test extends AnyFlatSpec {
     val person = Person("Peter", "Smith", "Manchester", 15)
 
     assert(!exercise002.canWatchFilm(person, 16))
+  }
+
+  //Consider the following test - if a 13 year old wanted to watch a 12 film.
+
+  //Try adding this test. Does it pass? If not how can you fix your canWatchFilm method so that it utilises that ageLimit variable:
+
+  "A person" should "be correctly identified as being able to watch an over 12 film" in {
+    val person = Person("Peter", "Smith", "Manchester", 13)
+
+    assert(exercise002.canWatchFilm(person, 12))
   }
 
 }
